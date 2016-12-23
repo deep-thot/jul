ccab2ceebd19c4884527d589ff17da9b175cd568
Jonatan <jonatan@deep-thot.se>
2016-12-23 11:11:14 +0100

    fucking frameworks...

---
+++ app/polyfills.ts
@@ -0,0 +1,9 @@
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+require('zone.js/dist/zone');
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import 'rxjs';
+
---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'polyfills': './app/polyfills.ts',
+        'app': './app/main.ts'
+    },
+
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+
+    output: {
+        path: './dist',
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file?name=assets/[name].[hash].[ext]'
+            }
+
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'polyfills']
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+};
